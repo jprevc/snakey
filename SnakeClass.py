5c68ad969185764f1c2ab73c4db94af91d924771
jprevc <jost.prevc@gmail.com>
2018-12-25 14:06:29 +0100

    major refactoring user is now able to change configuration by modifying config.json file

--- SnakeClass.py
+++ SnakeClass.py
@@ -1,16 +1,16 @@
 import pygame
-
+import random
 
 class Snake:
-
-    block_size = 10
 
     def __init__(self,
                  start_pos,
                  move_keys,
                  color,
-                 num_of_start_blocks=3):
+                 block_size,
+                 num_of_start_blocks):
 
+        self.block_size = block_size
         self.start_pos = start_pos
         self.move_keys = move_keys
         self.color = color
@@ -22,7 +22,7 @@ class Snake:
         # set first start blocks
         self.block_pos_lst = []
         for i in range(num_of_start_blocks):
-            self.block_pos_lst.append((self.start_pos[0] - i * Snake.block_size, self.start_pos[1]))
+            self.block_pos_lst.append((self.start_pos[0] - i * self.block_size, self.start_pos[1]))
 
     def get_dir_from_keystack(self):
         if self.key_stack:
@@ -48,26 +48,61 @@ class Snake:
 
     def set_new_state(self, game_dims, snakes_lst):
         # add new block to front of snake according to direction
-        new_block = [(self.block_pos_lst[0][0] + self.curr_dir[0]*Snake.block_size,
-                      self.block_pos_lst[0][1] + self.curr_dir[1]*Snake.block_size)]
+        new_block = [(self.block_pos_lst[0][0] + self.curr_dir[0]*self.block_size,
+                      self.block_pos_lst[0][1] + self.curr_dir[1]*self.block_size)]
         self.block_pos_lst = new_block + self.block_pos_lst
 
         # remove last block from snake
         self.block_pos_lst.pop()
 
-        # check for collision with screen frame
-        self.collision = (self.block_pos_lst[0][0] > game_dims[0] or self.block_pos_lst[0][1] > game_dims[1]) or \
-                         (self.block_pos_lst[0][0] < 0 or self.block_pos_lst[0][1] < 0)
-        if self.collision:
-            return
+        # check for collision with screen frame or with other snakes
+        # get list of snakes with self removed from it
+        othr_snake_lst = [snake for snake in snakes_lst if snake is not self]
+        if self.is_frame_collision(game_dims) or self.is_snake_collision(othr_snake_lst):
+            self.collision = True
+        else:
+            self.collision = False
 
-        # check for collision with snakes
-        for snake in snakes_lst:
-            if snake is self:  # check for collision with itself
-                self.collision = self.block_pos_lst[0] in self.block_pos_lst[1:]
-                if self.collision:
-                    return
-            else:  # check for collision with other snakes
-                self.collision = self.block_pos_lst[0] in snake.block_pos_lst
-                if self.collision:
-                    return
+    def is_snake_collision(self, other_snakes):
+        """ Returns True if snake is in collision with itself or other snakes. """
+        # check for collision with itself
+        if self.block_pos_lst[0] in self.block_pos_lst[1:]:
+            return True
+
+        # check for collision with other snakes
+        for snake in other_snakes:
+            if self.block_pos_lst[0] in snake.block_pos_lst:
+                return True
+
+        return False
+
+    def is_frame_collision(self, game_dims):
+        """ Returns True if snake is in collision with game frame. """
+        return not ((0 <= self.block_pos_lst[0][0] < game_dims[0]) and
+                    (0 <= self.block_pos_lst[0][1] < game_dims[1]))
+
+
+class Cherry:
+
+    def __init__(self, block_size):
+        self.block_size = block_size
+        self.position = None
+
+    def _is_cherry_position_valid(self, snake_lst):
+        """ Checks that cherry position is not placed onto some snake. """
+        for snake in snake_lst:
+            if self.position in snake.block_pos_lst:
+                return False
+
+        return True
+
+    def set_new_random_position(self, snake_lst, frame_size):
+        """ Sets new random position for cherry. """
+
+        self.position = (random.randrange(0, frame_size[0], self.block_size),
+                         random.randrange(0, frame_size[1], self.block_size))
+
+        # recursively call function until new cherry position is valid
+        if not self._is_cherry_position_valid(snake_lst):
+            self.set_new_random_position(snake_lst, frame_size)
+
--- snake.py
+++ snake.py
@@ -1,21 +1,18 @@
 import sys
 import pygame
 import random
-from SnakeClass import Snake
-
-pygame.init()
-
-size = width, height = 640, 480
-black = 0, 0, 0
-
-refresh_rate = 100
-
-screen = pygame.display.set_mode(size)
-
-start_pos = 300, 200
-
+from SnakeClass import Snake, Cherry
+import json
 
 def set_new_cherry_pos(snake_lst):
+    """
+    Sets new cherry position.
+
+    :param snake_lst: List, containing all snake instances present in the game. This is needed
+                      to check that cherry will not be placed onto a snake.
+    :type snake_lst: list of Snake
+    """
+
     new_cherry_pos = random.randrange(0, width, Snake.block_size), random.randrange(0, height, Snake.block_size)
 
     # check if new cherry position is within any of the snakes and set new one
@@ -26,69 +23,127 @@ def set_new_cherry_pos(snake_lst):
     return new_cherry_pos
 
 
-def init_game():
+def init_game(config_data):
+    """
+    Initializes the game.
+    """
+    # colors for snakes
+    snake_colors = [(0, 255, 0),  # player 1 is green
+                    (0, 0, 255),  # player 2 is blue
+                    (255, 255, 50),  # player 3 is yellow
+                    (205, 0, 205)]  # player 4 is purple
 
-    snake1 = Snake(start_pos=(300, 200),
-                   move_keys={'up': pygame.K_UP,
-                              'right': pygame.K_RIGHT,
-                              'down': pygame.K_DOWN,
-                              'left': pygame.K_LEFT},
-                   color=(0, 255, 0),
-                   num_of_start_blocks=10)
+    # create snake instances
+    init_snake_lst = []
+    for i in range(config_data["num_snakes"]):
+        keys = config_data["keys"][i]
+        snake = Snake(start_pos=config_data["start_pos"][i],
+                      move_keys={'up': pygame.__getattribute__(keys[0]),
+                                 'right': pygame.__getattribute__(keys[1]),
+                                 'down': pygame.__getattribute__(keys[2]),
+                                 'left': pygame.__getattribute__(keys[3])},
+                      color=snake_colors[i],
+                      block_size=config_data["block_size"],
+                      num_of_start_blocks=config_data["initial_snake_length"])
 
-    snake2 = Snake(start_pos=(300, 300),
-                   move_keys={'up': pygame.K_w,
-                              'right': pygame.K_d,
-                              'down': pygame.K_s,
-                              'left': pygame.K_a},
-                   color=(0, 0, 255),
-                   num_of_start_blocks=10)
-    init_snake_lst = [snake1, snake2]
+        init_snake_lst.append(snake)
 
-    new_cherry_pos = set_new_cherry_pos(init_snake_lst)
+    # create cherry instances
+    init_cherry_lst = []
+    for i in range(config_data["num_cherries"]):
+        cherry = Cherry(block_size)
+        cherry.set_new_random_position(init_snake_lst, config_data["main_window_size"])
+        init_cherry_lst.append(cherry)
 
-    return init_snake_lst, new_cherry_pos
+    return init_snake_lst, init_cherry_lst
+
+def redraw_screen(snake_lst, cherry_lst, block_size):
+    # clear screen
+    screen.fill(BLACK)
+
+    # draw snakes
+    for snake in snake_lst:
+        for block_pos in snake.block_pos_lst:
+            pygame.draw.rect(screen,
+                             snake.color,
+                             (block_pos[0], block_pos[1], block_size, block_size))
+
+    # draw cherries
+    for cherry in cherry_lst:
+        pygame.draw.rect(screen,
+                         (255, 0, 0),
+                         (cherry.position[0], cherry.position[1], block_size, block_size))
+
+    # update display
+    pygame.display.update()
+
+def main_loop(snake_list, cherry_list):
+    while 1:
+        # capture events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # happens when user tries to close window
+                sys.exit()  # exit from game
+            elif event.type == pygame.KEYDOWN:
+                # happens on key pressed
+                # check which snake's key was pressed and add it to key stack
+                for snake in snake_list:
+                    if event.key in [val for _, val in snake.move_keys.items()]:
+                        snake.key_stack.append(event.key)
+            elif event.type == pygame.USEREVENT:  # happens on each timer tick
+
+                for snake in snake_list:
+                    snake.get_dir_from_keystack()
+                    snake.set_new_state(size, snake_list)
+
+                    # check if there is collision
+                    if snake.collision:
+                        return SnakeGameStatusFlags.COLLISION_OCCURENCE
+
+                    # check if any of the cherries was eaten by the current snake
+                    for cherry in cherry_list:
+                        if snake.block_pos_lst[0] == cherry.position:
+                            # append new block to snake that ate the cherry
+                            snake.block_pos_lst.append(snake.block_pos_lst[-1])
+
+                            # set new random position for the eaten cherry
+                            cherry.set_new_random_position(snake_lst, size)
+
+                # redraw screen with updated snake and cherry positions
+                redraw_screen(snake_list, cherry_list, block_size)
 
 
-pygame.time.set_timer(pygame.USEREVENT, refresh_rate)
+class SnakeGameStatusFlags:
+    COLLISION_OCCURENCE = 1
 
-timer = pygame.time.get_ticks()
 
-snake_lst, cherry_pos = init_game()
-# key_stack = [pygame.key.get_pressed()]
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            for snake in snake_lst:
-                if event.key in [val for _, val in snake.move_keys.items()]:
-                    snake.key_stack.append(event.key)
-        elif event.type == pygame.USEREVENT:
+if __name__ == '__main__':
+    pygame.init()
 
-            for snake in snake_lst:
-                snake.get_dir_from_keystack()
-                snake.set_new_state(size, snake_lst)
+    # load configuration data
+    with open('config.json', 'r') as config_file:
+        configuration_data = json.load(config_file)
 
-                # check if there is collision
-                if snake.collision:
-                    snake_lst, cherry_pos = init_game()
+    size = width, height = configuration_data["main_window_size"]
+    BLACK = 0, 0, 0
+    refresh_rate = configuration_data["refresh_rate"]
+    start_pos = configuration_data["start_pos"]
+    block_size = configuration_data["block_size"]
 
-                # check if cherry was eaten
-                if snake.block_pos_lst[0] == cherry_pos:
-                    snake.block_pos_lst.append(snake.block_pos_lst[-1])
-                    cherry_pos = set_new_cherry_pos(snake_lst)
+    # set display
+    screen = pygame.display.set_mode(size)
 
-            screen.fill(black)
+    # set timer
+    pygame.time.set_timer(pygame.USEREVENT, refresh_rate)
 
-            # draw snake
-            for snake in snake_lst:
-                for block_pos in snake.block_pos_lst:
-                    pygame.draw.rect(screen,
-                                     snake.color,
-                                     (block_pos[0], block_pos[1], Snake.block_size, Snake.block_size))
+    timer = pygame.time.get_ticks()
 
-            # draw cherry
-            pygame.draw.rect(screen, (255, 0, 0), (cherry_pos[0], cherry_pos[1], Snake.block_size, Snake.block_size))
+    while True:
+        # initialize new game
+        snake_lst, cherry_pos = init_game(configuration_data)
 
-            pygame.display.update()
+        # main loop will exit only if collision occurs
+        main_loop(snake_lst, cherry_pos)
+
+
+
